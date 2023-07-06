bcd7562072b8911b56218bc3044939af3a14723c
Hanson Addo <addhansen96@gmail.com>
2023-07-05 23:22:41 -0400

    Deployed website

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -52,16 +52,16 @@ const Contact = () => {
                         <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
-                                    <input placeholder='Name' type='text' name='name'  required />
+                                    <input placeholder='Your Name' type='text' name='name'  required />
                                 </li>
                                 <li className='half'>
-                                    <input placeholder='Email'  type='email' name='email' required />
+                                    <input placeholder='Your Email'  type='email' name='email' required />
                                 </li>
                                 <li>
-                                    <input placeholder="Subject" type="text" name="subject" required />
+                                    <input placeholder="The Subject" type="text" name="subject" required />
                                 </li>
                                 <li>
-                                    <textarea placeholder="Message" name="message" required></textarea>
+                                    <textarea placeholder="The Message" name="message" required></textarea>
                                 </li>
                                 <li>
                                     <input type='submit' className='flat-button' value='SEND'/>
